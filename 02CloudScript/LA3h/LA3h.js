e133df8cd35652f8968e49af56c06839eae51167
yangxiang-eng <54496351+yangxiang-eng@users.noreply.github.com>
2025-08-07 13:15:28 +0800

    Update LA3h.js

--- 02CloudScript/LA3h/LA3h.js
+++ 02CloudScript/LA3h/LA3h.js
@@ -21,7 +21,7 @@ function decodeUplink(input) {
             let data = {
                 report: true,
                 flag: bytes[2],    // report type
-                temp: toSignedInt16((bytes[3] << 8) | bytes[4]), // temp1
+                temp: toSignedInt16((bytes[3] << 8) | bytes[4]) / 10.0, // temp1
 				hum: bytes[5]
             };
             results.push(data);
@@ -49,7 +49,7 @@ function decodeUplink(input) {
                         report_history: true,
                         measure_utc_time: (bytes[index + 1] << 24) | (bytes[index + 2] << 16) | (bytes[index + 3] << 8) | bytes[index + 4],
                         flag: bytes[index + 5],
-                        temp: toSignedInt16((bytes[index + 6] << 8) | bytes[index + 7]),
+                        temp: toSignedInt16((bytes[index + 6] << 8) | bytes[index + 7]) / 10.0,
 						hum: bytes[index + 8]
                     };
                     results.push(data);
